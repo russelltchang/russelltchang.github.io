fd56b1ea9e0816b2aff96eb095b057b95576aac2
Russell <Russell@Russells-MacBook-Pro.local>
2018-04-22 17:40:31 -0700

    separating files

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  $("#about").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".about").offset().top-10 // -10 to respect padding
+    }, 400);
+  });
+    
+  $("#portfolio").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".portfolio").offset().top 
+    }, 800);
+  }); 
+    
+  $("#contact").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".form").offset().top
+    }, 1000);
+  });
+    
+});
