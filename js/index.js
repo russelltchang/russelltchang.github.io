d7adb6e5ae6ea483d89c7a4244bdde20010a51e8
Russell <Russell@Russells-MacBook-Pro.local>
2018-05-01 16:00:52 -0700

    layout changes

--- js/index.js
+++ js/index.js
@@ -1,27 +1,34 @@
-$(document).ready(function() {
+$("#down").click(function() {
+  $('html, body').animate({
+    scrollTop: $(".projects").offset().top 
+  }, 800);
+}); 
 
-  $("#down").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".portfolio").offset().top 
-    }, 800);
-  }); 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-  $("#about").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".about").offset().top-10 // -10 to respect padding
-    }, 400);
-  });
-    
-  $("#portfolio").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".portfolio").offset().top 
-    }, 800);
-  }); 
-    
-  $("#contact").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".contact").offset().top
-    }, 1000);
-  });
-    
-});
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
