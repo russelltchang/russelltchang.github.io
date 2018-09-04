1532fc777da96c929549e2546353e850ea94c782
Russell <Russell@Russells-MacBook-Pro.local>
2018-09-04 14:44:06 -0700

    revisions

--- js/index.js
+++ js/index.js
@@ -1,28 +1,45 @@
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+$("#down").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#projects").offset().top
+  }, 1000);
+});
+
+$("#about").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#about")
+  }, 1000);
+});
+
+$("#work").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#projects").offset().top
+  }, 1000);
+});
+
+$("#contact").click(function() {
+  $('html, body').animate({
+    scrollTop: $("footer").offset().top
+  }, 1500);
+});
+
+var animateHTML = {
+  init: function() {
+    this.addEventHandlers();
+  },
+  addEventHandlers : function() {
+    window.addEventListener('scroll', this.checkPosition)
+    window.addEventListener('resize', this.init)
+  },
+  checkPosition : function() {
+    var elems = document.getElementsByClassName("hidden");
+    var h = window.innerHeight;
+    for (var i=0; i<elems.length; i++) {
+      var posFromTop = elems[i].getBoundingClientRect().top
+      if (posFromTop - h <= 0) {
+        elems[i].className = elems[i].className.replace('hidden', 'images')
+      }
+    }
+  } 
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
+animateHTML.addEventHandlers();
