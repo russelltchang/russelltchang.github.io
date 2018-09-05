3a53e685723ddef3964c1e51d19abb146fd82a93
Russell <Russell@Russells-MacBook-Pro.local>
2018-09-05 16:20:54 -0700

    about section

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 $("#down").click(function() {
   $('html, body').animate({
+    scrollTop: $("#about").offset().top
+  }, 1000);
+});
+
+$("#anchor1").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#about").offset().top
+  }, 1000);
+});
+
+$("#anchor2").click(function() {
+  $('html, body').animate({
     scrollTop: $("#projects").offset().top
   }, 1000);
 });
 
-$("#about").click(function() {
-  $('html, body').animate({
-    scrollTop: $("#about")
-  }, 1000);
-});
-
-$("#work").click(function() {
-  $('html, body').animate({
-    scrollTop: $("#projects").offset().top
-  }, 1000);
-});
-
-$("#contact").click(function() {
+$("#anchor3").click(function() {
   $('html, body').animate({
     scrollTop: $("footer").offset().top
   }, 1500);
