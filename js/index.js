5b5628a56d0296ab590839e0327d3514fa7d377d
Russell <Russell@Russells-MacBook-Pro.local>
2018-09-04 19:01:33 -0700

    add favicon

--- js/index.js
+++ js/index.js
@@ -21,25 +21,3 @@ $("#contact").click(function() {
     scrollTop: $("footer").offset().top
   }, 1500);
 });
-
-var animateHTML = {
-  init: function() {
-    this.addEventHandlers();
-  },
-  addEventHandlers : function() {
-    window.addEventListener('scroll', this.checkPosition)
-    window.addEventListener('resize', this.init)
-  },
-  checkPosition : function() {
-    var elems = document.getElementsByClassName("hidden");
-    var h = window.innerHeight;
-    for (var i=0; i<elems.length; i++) {
-      var posFromTop = elems[i].getBoundingClientRect().top
-      if (posFromTop - h <= 0) {
-        elems[i].className = elems[i].className.replace('hidden', 'images')
-      }
-    }
-  } 
-}
-
-animateHTML.addEventHandlers();
