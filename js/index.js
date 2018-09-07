00b99bf688b6cdc8ca891e49bdebea0689633043
Russell <Russell@Russells-MacBook-Pro.local>
2018-09-07 11:23:33 -0700

    text ease in

--- js/index.js
+++ js/index.js
@@ -21,3 +21,13 @@ $("#anchor3").click(function() {
     scrollTop: $("footer").offset().top
   }, 1500);
 });
+
+$(window).scroll(function() {
+  $('#summary').each(function() {
+    var bottom_of_object = $(this).position().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    if (bottom_of_window > bottom_of_object ) {
+      $(this).addClass("animate");     
+    }   
+  }); 
+});
