a8c60a22867f299967f5f2343d9fae332d2d54f9
Russell <Russell@Russells-MacBook-Pro.local>
2020-10-17 10:34:04 -0700

    portfolio page redesign

--- js/index.js
+++ js/index.js
@@ -1,40 +1,11 @@
-$("#down").click(function() {
-  $('html, body').animate({
-    scrollTop: $("#about").offset().top
-  }, 1000);
-});
-
 $("#anchor1").click(function() {
-  $('html, body').animate({
-    scrollTop: $("#about").offset().top
-  }, 1000);
-});
-
-$("#anchor2").click(function() {
   $('html, body').animate({
     scrollTop: $("#projects").offset().top
   }, 1000);
 });
 
-$("#anchor3").click(function() {
+$("#anchor2").click(function() {
   $('html, body').animate({
     scrollTop: $("footer").offset().top
   }, 1500);
-});
-
-$(window).scroll(function() {
-  $('#summary').each(function() {
-    var bottom_of_object = $(this).position().top + $(this).outerHeight();
-    var bottom_of_window = $(window).scrollTop() + $(window).height();
-    if (bottom_of_window > bottom_of_object ) {
-      $(this).addClass("animate");     
-    }   
-  }); 
-  $('#headshot').each(function() {
-    var bottom_of_object = $(this).position().top + $(this).outerHeight();
-    var bottom_of_window = $(window).scrollTop() + $(window).height();
-    if (bottom_of_window > bottom_of_object ) {
-      $(this).addClass("animateImage");     
-    }   
-  }); 
 });
